1e814daef680fb8767bbcaf6549b33c67439e017
ikari <sydnazar08@gmail.com>
2025-02-05 19:10:26 +0200

    Update Program.cs
    class book

--- Program.cs
+++ Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace Final_Project
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public class Book
         {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string Genre { get; set; }
+            public int Year { get; set; }
+            public string Id { get; set; }
+        }
+            static void Main(string[] args)
+        {
+           
         }
     }
 }
