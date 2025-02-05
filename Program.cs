6191f5da08bf6cb65632b034b4cd90c931a0e125
ikari <sydnazar08@gmail.com>
2025-02-05 20:55:05 +0200

    Update Program.cs
    classs

--- Program.cs
+++ Program.cs
@@ -28,28 +28,58 @@ namespace Final_Project
             public override string ToString()
             {
                 return $"{Id}: {Title} by {Author} ({Year}) - {(IsAvailable ? "Available" : "Borrowed")}";
-            }
         }
-        public class User
-        {
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-            public string UserId { get; set; }
-            public User() { }
-            public User(string firstName, string lastName, string userId)
-            {
-                FirstName = firstName;
-                LastName = lastName;
-                UserId = userId;
-            }
-            public override string ToString()
-            {
-                return $"{UserId}: {FirstName} {LastName}";
-            }
-        }
-    internal class Program
+    }
+    public class User
     {
-        static void Main(string[] args)
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserId { get; set; }
+        public User() { }
+        public User(string firstName, string lastName, string userId)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            UserId = userId;
+        }
+        public override string ToString()
+        {
+            return $"{UserId}: {FirstName} {LastName}";
+        }
+    }
+    public class Library
+    {
+        private static Library _instance;
+        private List<Book> books;
+        private List<User> users;
+        private Dictionary<string, List<Book>> borrowedBooks;
+        private const string BooksFile = "books.xml";
+        private const string UsersFile = "users.xml";
+        private const string LogFile = "log.txt";
+
+        private Library()
+        {
+            books = LoadData<List<Book>>(BooksFile) ?? new List<Book>();
+            users = LoadData<List<User>>(UsersFile) ?? new List<User>();
+            borrowedBooks = new Dictionary<string, List<Book>>();
+        }
+        public static Library Instance => _instance ??= new Library();
+
+        public void AddBook(Book book)
+        {
+            if (books.Any(b => b.Id == book.Id))
+            {
+                Console.WriteLine("Книга с таким ID уже существует!");
+                return;
+            }
+            books.Add(book);
+            SaveData(books, BooksFile);
+            LogOperation($"Добавлена книга: {book}");
+        }
+    }
+        internal class Program
+    {
+            static void Main(string[] args)
             {
            
             }
